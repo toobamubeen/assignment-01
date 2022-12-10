c19e92569317e43d88d12d3464fb09ca56debcf4
toobamubeen <toobamubeenyasin@gmail.com>
2022-12-10 21:20:06 +0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Assignment 01</title>
+      </Head>
+      <h1>Saylani Web and Mobile - Batch1</h1>
+      <h2>Assignment No. 1</h2>
+      <h3>Techloset Solutions-The Technology Closet</h3>
+      <h4>First HTML Site</h4>
+      <h5>Let'beign the journey</h5>
+      <h6>Learn and Grow together</h6>
+      <p>My name is tuba. I Study in collage . I want to learn about developing</p>
+    </div>
+  )
+}
